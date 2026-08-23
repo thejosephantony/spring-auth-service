6ad35b3c42675c815145f67d5438a326b5d00a79
Joseph Antony <161381453+thejosephantony@users.noreply.github.com>
2026-08-23 12:46:36 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/joseph/spring_auth_service/SpringAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joseph.spring_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseph/spring_auth_service/SpringAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseph.spring_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
